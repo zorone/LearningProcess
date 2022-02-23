8ea9fc4a4e2435fa2b6f9ae0d68a0a25210f0346
Imron  Jehleh <imron.jehleh@gmail.com>
2022-02-23 17:34:54 +0700

    added code for testing problem.

--- 5.c
+++ 5.c
@@ -8,7 +8,7 @@ int main()
     for(char i1=65; i1<91; i1++){
         for(char i2=48; i2<58; i2++){
             for(char i3=48; i3<58; i3++){
-                for(char i4=49; i4<58; i4++){
+                for(char i4=48; i4<58; i4++){
                     printf("%c-%c%c%c\n", i1, i2, i3, i4);
                     counter += 1;
                 }
