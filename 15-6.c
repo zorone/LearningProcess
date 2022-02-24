9ed247044f52a6d7b19486f936d0ad42931937a9
Imron  Jehleh <imron.jehleh@gmail.com>
2022-02-24 12:22:50 +0700

    Improve performance (maybe?).

--- 15-6.c
+++ 15-6.c
@@ -45,6 +45,7 @@ int main(){
                                     for(n5.num1=0; n5.num1<10; n5.num1++){
                                         if(nearestNeighbor(n4.num1, n4.num2, n5.num1)) continue;
                                         for(n5.num2=0; n5.num2<10; n5.num2++){
+                                            if(nearestNeighbor(n4.num2, n5.num1, n5.num2)) continue;
                                             if(dup(n1.num1, n1.num2, n2.num1, n2.num2, n3.num1, n3.num2, n4.num1, n4.num2, n5.num1, n5.num2)) continue;
                                             printf("%10lld %u%u%u%u%u%u%u%u%u%u\n", ctr, n1.num1, n1.num2, n2.num1, n2.num2, n3.num1, n3.num2, n4.num1, n4.num2, n5.num1, n5.num2);
                                             ctr+=1;
@@ -105,9 +106,7 @@ char dup(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigne
     if(g == h) return 1;
     if(g == i) return 1;
     if(g == j) return 1;
-    if(h == i) return 1;
     if(h == j) return 1;
-    if(i == j) return 1;
 
     return 0;
 
