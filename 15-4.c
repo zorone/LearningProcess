62458fe2091fe907bf1b58689d4cbdb99d32dd2a
Imron  Jehleh <imron.jehleh@gmail.com>
2022-02-24 11:47:04 +0700

    Added counter.

--- 15-4.c
+++ 15-4.c
@@ -24,6 +24,8 @@ int main(){
     n5.num1 = 0;
     n5.num2 = 0;
 
+    long long int ctr=0;
+
     for(n1.num1=0; n1.num1<10; n1.num1++){
         dup(n1.num1, n1.num2, n2.num1, n2.num2, n3.num1, n3.num2, n4.num1, n4.num2, n5.num1, n5.num2);
         for(n1.num2=0; n1.num2<10; n1.num2++){
@@ -35,7 +37,8 @@ int main(){
                                 for(n4.num2=0; n4.num2<10; n4.num2++){
                                     for(n5.num1=0; n5.num1<10; n5.num1++){
                                         for(n5.num2=0; n5.num2<10; n5.num2++){
-                                            printf("%u%u%u%u%u%u%u%u%u%u\n", n1.num1, n1.num2, n2.num1, n2.num2, n3.num1, n3.num2, n4.num1, n4.num2, n5.num1, n5.num2);
+                                            printf("%10lld %u%u%u%u%u%u%u%u%u%u\n", ctr, n1.num1, n1.num2, n2.num1, n2.num2, n3.num1, n3.num2, n4.num1, n4.num2, n5.num1, n5.num2);
+                                            ctr+=1;
                                         }
                                     }
                                 }
