5e64013400ffebe3b00765ce94b40d7140f1b89b
Imron  Jehleh <imron.jehleh@gmail.com>
2022-02-24 10:54:40 +0700

    Rewrote code.

--- 15-4.c
+++ 15-4.c
@@ -1,85 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DEFAULT "0123456789"
+int main(){
 
-int calc(FILE *fptr);
-int restoreDefault(char *ptr)
-
-int deepen(int level, int value);
-
-int padding(int level, FILE *fptr);
-int newLine(FILE *fptr);
-
-int writeFile();
-
-int main(){   
-
-    writeFile();
-
-    return 0;
-}
-
-
-int writeFile(){
-
-    FILE *fp1;
-
-    fp1 = fopen("./result-15-4.txt", "w");
-
-    char invalidString[] = {"\0"};
-    char unusedString[] = {"0123456789"};
-    char *ptr1 = &invalidString;
-    char *ptr2 = &unusedString;
-
-    int lvr = 0;
-    int value = 0;
-    int *val = &value;
-
-    char still = 1;
-
-    while(still){
-
-        lvr = calc(lvr, val, ptr1, ptr2, &still);
-
-    }
-
-    fclose(fp1);
-
-    return 0;
-}
-
-int calc(int level, int *data, char *sp1, char *sp2){
-    char valid[];
-    char invalid[];
-    int depth = level;
-    for(int i=0; i<10; i++){
-        if(depth!=9){
-            calc(depth+1, data, sp1, sp2);
+    for(char a=0; a<10; a++){
+        for(char b=0; b<10; b++){
+            for(char c=0; b<10; b++){
+                for(char d=0; b<10; b++){
+                    for(char e=0; b<10; b++){
+                        for(char f=0; b<10; b++){
+                            for(char g=0; b<10; b++){
+                                for(char h=0; b<10; b++){
+                                    for(char i=0; b<10; b++){
+                                        for(char j=0; b<10; b++){
+                                            printf("%d%d%d%d%d%d%d%d%d%d\n",a, b, c, d, e, f, g, h, i, j);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
-    return depth;
-}
-
-int padding(int level, FILE *fptr){
-    for(int lv=0; lv<lvr: lv++){
-        fprintf(fptr, "  ");
-    }
-
     return 0;
-}
-
-int newLine(FILE *fptr){
-    fprintf(fptr, "\n");
-
-    return 0;
-}
-
-int restoreDefault(char *ptr){
-
-    strcpy(ptr, DEFAULT);
-
-    return 0;
-
 }
